6516c8e70f1a6624ddf0c58be78001e800352583
KMirfan <irfanarimbra@gmail.com>
2022-11-29 23:50:38 +0530

    shorts added

--- common/urls.py
+++ common/urls.py
@@ -1,8 +1,10 @@
 
 from django.urls import path
 from .import views
+app_name='common'
 
 urlpatterns = [
     
-    path('', views.common_home),
+    path('', views.common_home, name='index'),
+     path('shorts', views.common_shorts,name='shorts'),
 ]
--- common/views.py
+++ common/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 def common_home(request):
     return render(request,'youtubetemp/index.html')
+def common_shorts(request):
+    return render(request,'youtubetemp/shorts.html')
